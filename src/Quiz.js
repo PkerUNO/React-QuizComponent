84edfe413fcdaae63275aa0af2e4975041d22476
Richard Whittaker <richard.whittaker@bbc.co.uk>
2018-08-29 16:46:50 +0100

    Complete Module 5

--- src/Quiz.js
+++ src/Quiz.js
@@ -19,9 +19,13 @@ class Quiz extends Component {
 
         return(
             <div>
-                {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
+                {isQuizEnd ? <QuizEnd /> : <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
             </div>
         );
+    }
+
+    showNextQuestion() {
+        this.setState({quiz_position: this.state.quiz_position + 1});
     }
 }
 export default Quiz;
